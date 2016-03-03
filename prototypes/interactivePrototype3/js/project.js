6c6fb1eef174a6489590a81f58a1be63c231e1f0
samanning <samanning@ucdavis.edu>
2016-03-03 01:34:48 -0800

    add audience test materials and audio clips

--- prototypes/interactivePrototype3/js/project.js
+++ prototypes/interactivePrototype3/js/project.js
@@ -3,28 +3,36 @@
 //call to jQuery ready function
 //make sure html and css are loaded
 
-var myAudio = document.createElement('audio');
-myAudio.controls = true;
-myAudio.src = 'media/clink.mp3';
+var audioCorrect = document.createElement('audio');
+var audioIncorrect = document.createElement('audio');
+audioCorrect.controls = true;
+audioCorrect.src = 'media/correct.mp3';
+audioIncorrect.controls = true;
+audioIncorrect.src = 'media/wrong.mp3';
 
 
 $(document).ready(function(){
     
     console.log('ready');
+
     
-    //click event to start
-    //$('#home').click(function(){
-        //call function to go to nav
-        
-    //
-    
-    $('object').draggable({
-        cursor: 'grab';
+    $('.object').draggable({
+        cursor: 'grab',
         opacity: 0.6,
     })
-    $('.bin').droppable({
+    $('.binC').droppable({
         drop: function (event, ui){
-            myAudio.play();
+            audioCorrect.play();
         }
-    });
+    })
+    $('.binR').droppable({
+        drop: function (event, ui){
+            audioIncorrect.play();
+        }
+    })
+     $('.binL').droppable({
+        drop: function (event, ui){
+            audioIncorrect.play();
+        }
+    })
 })
